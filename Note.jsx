f241c38503040f18e56826abeec7d26ca087f9a1
Sandhyajunnu <78165254+Sandhyajunnu@users.noreply.github.com>
2021-06-06 13:52:57 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Hello Eachone,</h1>
+      <p>
+        I'm Sandya Junnu i glad to be part of Shape AI bootcamps. By attending
+        these bootcamps i gained knowledge Thank you Shourya Sir and Shape AI
+        team.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
